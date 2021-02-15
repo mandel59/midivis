64f9e6a213d58787f07b6a680b7aafcb58e5198f
Ryusei Yamaguchi <mandel59@gmail.com>
2021-02-16 00:45:27 +0900

    change navigate request to use cache with StaleWhileRevalidate

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,24 +1,12 @@
 import { registerRoute } from "workbox-routing"
 import {
-    NetworkFirst,
     StaleWhileRevalidate,
 } from "workbox-strategies"
 import { CacheableResponsePlugin } from "workbox-cacheable-response"
 
 registerRoute(
-    ({ request }) => request.mode === "navigate",
-    new NetworkFirst({
-        cacheName: "pages",
-        plugins: [
-            new CacheableResponsePlugin({
-                statuses: [200],
-            })
-        ]
-    }),
-)
-
-registerRoute(
-    ({ request }) => ["style", "script", "worker", "image", "manifest"].includes(request.destination),
+    ({ request }) =>
+        request.mode === "navigate" || ["style", "script", "worker", "image", "manifest"].includes(request.destination),
     new StaleWhileRevalidate({
         cacheName: "assets",
         plugins: [
