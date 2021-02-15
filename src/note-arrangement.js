47888c2fef88e4420aa084e59cb22e0fbdc4eccc
Ryusei Yamaguchi <mandel59@gmail.com>
2021-02-15 23:48:30 +0900

    correct capitalization

--- src/note-arrangement.js
+++ src/note-arrangement.js
@@ -1,6 +1,6 @@
 const noteArrangements = [
     { id: "fourth", label: "Fourth" },
-    { id: "c-system", label: "C-System" },
+    { id: "c-system", label: "C-system" },
 ]
 
 module.exports = { noteArrangements }
